1cbbbbe92de2e4bacff48433a6eadb328876923e
Hironori Sugino <git@sugi.cc>
2019-11-07 18:05:30 +0900

    add unity proj

---
+++ Assets/ObsRemote/Scripts/ObsControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using OBSWebsocketDotNet;
+using OBSWebsocketDotNet.Types;
+
+//https://github.com/Palakis/obs-websocket-dotnet/blob/master/TestClient/MainWindow.cs
+public class ObsControl : MonoBehaviour
+{
+    public string RemoteIP { set { remoteIp = value; } }
+    [SerializeField] string remoteIp = "localhost";
+    public string RemotePort { set { remotePort = int.Parse(value); } }
+    [SerializeField] int remotePort = 4444;
+    string url { get { return $"ws://{remoteIp}:{remotePort}"; } }
+    public string Password { set { password = value; } }
+    [SerializeField] string password;
+
+    [Header("UI Settings")]
+    public RectTransform connectedUI;
+    public Text connectBtnText;
+
+    OBSWebsocket obs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obs = new OBSWebsocket();
+
+        obs.Connected += (sender, args) =>
+        {
+            connectBtnText.text = "Dissconnect";
+            connectedUI.gameObject.SetActive(true);
+        };
+        obs.Disconnected += (sender, args) =>
+        {
+            connectBtnText.text = "Connect To OBS";
+            connectedUI.gameObject.SetActive(false);
+        };
+    }
+    private void OnDestroy()
+    {
+        obs.Disconnect();
+    }
+
+    public void OnConnectButton()
+    {
+        if (!obs.IsConnected)
+        {
+            try
+            {
+                obs.Connect(url, password);
+            }
+            catch (AuthFailureException)
+            {
+                Debug.Log("Authentication failed.");
+            }
+            catch (ErrorResponseException ex)
+            {
+                Debug.Log("Connect failed" + ex.Message);
+            }
+        }
+        else
+            obs.Disconnect();
+    }
+
+    public void OnStartRecordingButton() {
+        obs.StartRecording();
+    }
+    public void OnStopRecordingButton()
+    {
+        obs.StopRecording();
+    }
+}
---
+++ Assets/ObsRemote/Scripts/ViconControl.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Xml;
+
+public class ViconControl : MonoBehaviour
+{
+    public UdpSender sender;
+    public string RemoteIP
+    {
+        set
+        {
+            if (sender.remoteIp != value)
+                sender.CreateRemoteEP(value, sender.remotePort);
+        }
+    }
+    public string RemotePort
+    {
+        set
+        {
+            var newPort = int.Parse(value);
+            if (sender.remotePort != newPort)
+                sender.CreateRemoteEP(sender.remoteIp, newPort);
+        }
+    }
+
+    public string SubjectName { set { subjectName = value; } }
+    [SerializeField] string subjectName = "ViconTest";
+    public string Notes { set { notes = value; } }
+    [SerializeField] string notes = "";
+    public string Description { set { description = value; } }
+    [SerializeField] string description = "";
+    public string DbPath { set { dbPath = value; } }
+    [SerializeField] string dbPath = "D:\\ViconDB\\Reflap\\Project 2\\Gene\\Session 1\\";
+    public string Delay { set { delay = int.Parse(value); } }
+    [SerializeField] int delay = 0;
+
+    [SerializeField] int packetId = 0;
+
+    public void SendStartCapture()
+    {
+        SendText(CreateStartCaptureXML());
+    }
+
+    public void SendStopCapture()
+    {
+        SendText(CreateStopCaptureXML());
+    }
+
+
+    string CreateStartCaptureXML()
+    {
+        var doc = new XmlDocument();
+        var docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", "no");
+        doc.AppendChild(docNode);
+
+        var rootNode = doc.CreateElement("CaptureStart");
+        doc.AppendChild(rootNode);
+
+        var childNode = doc.CreateElement("Name");
+        childNode.SetAttribute("VALUE", subjectName);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("Notes");
+        childNode.SetAttribute("VALUE", notes);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("Description");
+        childNode.SetAttribute("VALUE", description);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("DatabasePath");
+        childNode.SetAttribute("VALUE", dbPath);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("Delay");
+        childNode.SetAttribute("VALUE", delay.ToString());
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("PacketID");
+        childNode.SetAttribute("VALUE", packetId.ToString());
+        rootNode.AppendChild(childNode);
+        packetId++;
+
+        return doc.OuterXml;
+    }
+    string CreateStopCaptureXML()
+    {
+        var doc = new XmlDocument();
+        var docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", "no");
+        doc.AppendChild(docNode);
+
+        var rootNode = doc.CreateElement("CaptureStop");
+        rootNode.SetAttribute("RESULT", "SUCCESS");
+        doc.AppendChild(rootNode);
+
+        var childNode = doc.CreateElement("Name");
+        childNode.SetAttribute("VALUE", subjectName);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("DatabasePath");
+        childNode.SetAttribute("VALUE", dbPath);
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("Delay");
+        childNode.SetAttribute("VALUE", delay.ToString());
+        rootNode.AppendChild(childNode);
+
+        childNode = doc.CreateElement("PacketID");
+        childNode.SetAttribute("VALUE", packetId.ToString());
+        rootNode.AppendChild(childNode);
+        packetId++;
+
+        return doc.OuterXml;
+    }
+
+    void SendText(string text)
+    {
+        sender.Send(text);
+    }
+}
